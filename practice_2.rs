6831bcf86f86616b0da357c2cc30c77d615919ed
munachianya <115806832+munachianya@users.noreply.github.com>
2023-01-28 18:04:55 +0100

    My 2nd commit

---
+++ practice_2.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+	println!(); // prints just a
+
+	println!("hello "); // prints 
+
+	println!("format {} arguments", "some"); // prints format some arguments
+
+	println!("My name is {}. I am NOT the class rep of stream 1", "Dagogo William-Jumbo"); // prints just a
+	
+	}
+
