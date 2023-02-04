7e899dae27f2c02c40530ba8be089e1fe0c165f6
munachianya <115806832+munachianya@users.noreply.github.com>
2023-02-04 08:44:52 +0100

    My 8th commit

--- main.rs
+++ main.rs
@@ -1,11 +1,16 @@
-// The iter() function values of all elements in an array.
-
 fn main() {
-    let arr:[i32;4] = [10,20,30,40];
-    println!("array is {:?}",arr);
-    println!("array size is :{}",arr.len());
 
-    for val in arr.iter(){
-        println!("value is: {}",val);
-    }
+    // mutable array
+    let mut colors = ["red", "green", "yellow", "white"];
+    println!("\nOriginal array = {:?}", colors);
+
+    //mutable slice
+    let sliced_colors = &mut colors[1..3];
+
+    println!("First slice = {:?}", sliced_colors);
+
+    //change the value of the original slice at the first index
+    sliced_colors[1] = "purple";
+
+    println!("Changed slice = {:?}", sliced_colors);
 }
